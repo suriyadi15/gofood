3b64932f2969bdf6a86876ec76b3e4a3c80baccb
suriyadi <suriyadi.mps@gmail.com>
2020-12-24 15:19:23 +0700

    fix bugs

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,3 @@ import GoFood from './gofood'
 const gf = new GoFood()
 
 export default gf
-
-// gf.getRestaurant('c3d8177e-177b-4cc2-8cca-edcaca02860d')
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
-
-
-gf.getRestaurants()
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
